d6d4c5d58967b3757fe48cef0ca910d898617587
saymonp <saymon.t@outlook.com>
2020-10-26 11:38:10 -0300

    Validação de dados

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import "express-async-errors";
 import path from "path";
 
 import "./database/connection";
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
@@ -10,5 +12,6 @@ app.use(express.json());
 
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
 app.listen(3333);
